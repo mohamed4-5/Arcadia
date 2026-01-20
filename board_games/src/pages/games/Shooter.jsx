c23e17c25eb9f3cc0db729edce2f08dd9ae12d2e
mohamed4-5 <mohammedhla4@gmail.com>
2026-01-20 21:50:00 +0200

    fix problems

--- board_games/src/pages/games/Shooter.jsx
+++ board_games/src/pages/games/Shooter.jsx
@@ -47,7 +47,7 @@ export default function Shooter() {
     ]);
 
     // 👇 cooldown أقل
-    setTimeout(() => setCanShoot(true), 150);
+    setTimeout(() => setCanShoot(true), 100);
   };
 
   /* ================= 🎹 CONTROLS ================= */
