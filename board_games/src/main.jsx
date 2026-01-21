5a977315665f7232d6e509ced80e35877159382a
mohamed4-5 <mohammedhla4@gmail.com>
2026-01-21 16:59:28 +0200

    fix vercel problems

--- board_games/src/main.jsx
+++ board_games/src/main.jsx
@@ -4,10 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
